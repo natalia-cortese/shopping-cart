0087298e3c8d47c36353c576cc7bea59f395ebdb
Nats <taku246@gmail.com>
2023-03-22 15:59:55 -0300

    Adding Login for tests_orders

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,15 +1,16 @@
 import requests
+import json
 from datetime import datetime
 from unittest import TestCase
 
 from sqlalchemy.exc import SQLAlchemyError
 
 from models.carts import CartModel
-from models.items import ItemsModel
 from models.orders import OrderModel
 from sqlite.db import db
 
 ENDPOINT = "https://127.0.0.1:5000/orders/{cart_id}"
+LOGIN_ENDPOINT = "https://127.0.0.1:5000/login"
 
 
 class testOrder(TestCase):
@@ -45,27 +46,41 @@ class testOrder(TestCase):
             return 404, "Product doesn't exist in DB."
         return order
 
+    def get_token(self):
+        headers = {'Content-type': 'application/json'}
+        login_data = {'username': 'Natalia', 'password': 'qwerty1234'}
+        token = requests.post(LOGIN_ENDPOINT, data=json.dumps(login_data), headers=headers)
+        return token
+
     def test_get_order(self):
         cart = self.create_dummy_cart()
-        url = ENDPOINT.format(cart.id)
+        url = ENDPOINT.format(cart_id=cart.id)
         response = requests.get(url)
 
         response_data = response.json()
 
         assert response.status_code == 201
         assert response_data["status"] == "in_progress"
-        assert response_data["id"] == cart.id
+        assert response_data["cart_id"] == cart.id
 
-        self.delete_dummy_cart(cart.id)
+        self.delete_dummy_order(response_data['id'])
 
     def test_create_order(self):
         cart = self.create_dummy_cart()
-        url = ENDPOINT.format(cart.id)
-        response = requests.post(url)
+        url = ENDPOINT.format(cart_id=cart.id)
+        token = self.get_token()
+
+        headers = {
+            'Content-type': 'application/json',
+            'Accept': 'text/plain',
+            'cart_id': str(cart.id),
+            'token': 'Bearer {}'.format(token)
+        }
+        response = requests.post(url, headers=headers)
 
         response_data = response.json()
         order = self.search_dummy_order(cart.id)
 
         assert response_data["id"] == order.id
         assert response_data["created"] == order.created
-        self.delete_dummy_cart(order.id)
+        self.delete_dummy_order(order.id)
